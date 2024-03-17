734f7511b4510f20aa4da126f51880b335215652
Adam Osusky <adam.osusky02@gmail.com>
2024-03-17 13:19:36 +0100

    main: general get retriever

--- src/mulmod/main.py
+++ src/mulmod/main.py
@@ -1,6 +1,6 @@
 import sys
 
-from mulmod.extract import PdfExtractor
+from mulmod.extract import Extractions, PdfExtractor
 from mulmod.logger import get_logger
 from mulmod.retrieve.retriever import RetrievalResult, Retriever
 from mulmod.summary import Summarizer
@@ -37,28 +37,67 @@ def print_relevant(retrieval: RetrievalResult) -> None:
         print()
 
 
-def retrieval_only(pdf_path: str) -> None:
+def remove_empty(extracts: Extractions) -> Extractions:
+    return Extractions(
+        texts=[e for e in extracts.texts if e.content != ""],
+        tables=[e for e in extracts.tables if e.content != ""],
+        images=extracts.images,
+    )
+
+
+def get_retriever(
+    max_characters: int,
+    new_after_n_chars: int,
+    combine_text_under_n_chars: int,
+    img_summary_num_words: int,
+    summarize_texts=False,
+    txt_summary_num_words: int = 50,
+) -> Retriever:
     extractor = PdfExtractor(
-        max_characters=600,
-        new_after_n_chars=550,
-        combine_text_under_n_chars=500,
+        max_characters=max_characters,
+        new_after_n_chars=new_after_n_chars,
+        combine_text_under_n_chars=combine_text_under_n_chars,
     )
     extractions = extractor.extract(filepath)
+    extractions = remove_empty(extractions)
 
     texts = [e.content for e in extractions.texts]
     tables = [e.content for e in extractions.tables]
 
     logger.info("Started generating summaries for images.")
-    image_summarizer = Summarizer(num_words=10)
+    image_summarizer = Summarizer(num_words=img_summary_num_words)
     image_summaries = image_summarizer.get_summary(extractions.images)
     logger.info("Finished generating summaries for images.")
 
-    logger.info("Started creating vector database for retrieval.")
     retriever = Retriever()
-    retriever.add_docs_from_texts(texts, texts)
-    retriever.add_docs_from_texts(tables, tables)
+
+    if summarize_texts:
+        text_summarizer = Summarizer(num_words=txt_summary_num_words)
+        logger.info("Started generating summaries for texts and tables.")
+        text_summaries = text_summarizer.get_summary(extractions.texts)
+        table_summaries = text_summarizer.get_summary(extractions.tables)
+        logger.info("Finished generating summaries for texts and tables.")
+    else:
+        text_summaries = texts
+        table_summaries = tables
+
+    logger.info("Started creating vector database for retrieval.")
+    retriever.add_docs_from_texts(text_summaries, texts)
+    retriever.add_docs_from_texts(table_summaries, tables)
     retriever.add_imgs_from_extract(image_summaries, extractions.images)
     logger.info("Finished creating vector database for retrieval.")
+
+    return retriever
+
+
+def retrieval_only(pdf_path: str) -> None:
+    retriever = get_retriever(
+        max_characters=600,
+        new_after_n_chars=550,
+        combine_text_under_n_chars=500,
+        img_summary_num_words=50,
+        summarize_texts=False,
+    )
 
     print(INTRO_RET_MSG)
     while True:
